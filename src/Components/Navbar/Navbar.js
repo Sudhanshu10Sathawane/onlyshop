2dcb4e8e29dfd2ef2ee86e1ee203f8ddd2fd7c79
sudhanshu10sathawane <sudhanshusathwane@gmail.com>
2023-11-12 14:21:28 +0530

    First commit

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar.css'
+export default function Navbar(){
+    return(
+        <div id="nav">
+        <a id="logo" href="#">OnlyShop</a>
+        <ul id="menu">
+            <li><a href="#">🏠Home</a></li>
+            <li><a href="#">🛒Cart</a></li>
+        </ul>
+    </div>
+    );
+}
